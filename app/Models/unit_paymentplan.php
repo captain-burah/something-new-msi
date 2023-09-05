261bbae4713cd30edaf8a3d0e2de8d470f059fe0
Captain-Burah <cptburah@gmail.com>
2023-09-05 17:45:04 +0400

    Unit Migration

---
+++ app/Models/unit_paymentplan.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class unit_paymentplan extends Model
+{
+    use HasFactory;
+}
